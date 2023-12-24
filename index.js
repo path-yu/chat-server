01c388afc37cefbdcb865bf25c706baa3adf6aee
path-yu <1974675011@qq.com>
2023-12-24 21:14:49 +0800

    add ws

--- index.js
+++ index.js
@@ -2,52 +2,9 @@ const express = require("express");
 const app = express();
 const product = require("./api/product");
 const PORT = process.env.PORT || 3001;
+require("./websocket");
+app.use("/api/product", product);
 
-const mongoose = require("mongoose");
-const schema = new mongoose.Schema({ name: String, size: String });
-
-function run() {
-  mongoose
-    .connect(
-      "mongodb+srv://react:FZyg0RnKMINyjL9d@cluster0.h72djcl.mongodb.net/?retryWrites=true&w=majority"
-    )
-    .then(() => {
-      console.log(
-        "Pinged your deployment. You successfully connected to MongoDB!"
-      );
-      app.use("/api/product", product);
-      app.get("/test", async (req, res) => {
-        const Tank = mongoose.model("Tank", schema);
-
-        const small = new Tank({ size: "small" });
-        await small.save();
-        // or
-        await Tank.create({ size: "small", name: "八九式" });
-        // or, for inserting large batches of documents
-        await Tank.insertMany([{ size: "small" }]);
-        res.json({
-          status: 200,
-          message: "Get data has successfully",
-        });
-      });
-      app.get("/all", async (req, res) => {
-        const Tank = mongoose.model("Tank", schema);
-        let list = await Tank.find().lean();
-        res.json({
-          status: 200,
-          message: "Get data has successfully",
-          data: list,
-        });
-      });
-    })
-    .catch((err) => {
-      console.log(err, "err");
-    })
-    .finally(() => {
-      console.log("end");
-    });
-}
-run();
 app.use(express.json({ extended: false }));
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
