6421d1f13dc77fa686fe30d7481b1ad5d72d1d00
path-yu <1974675011@qq.com>
2023-12-31 20:53:14 +0800

    update version

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 
 import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { db } from "./firebase.ts";
-import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import {
   collection,
   doc,
